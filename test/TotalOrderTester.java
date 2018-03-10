8b5a2ed625bac5c0e24b92c2d443c0ed40421551
Kalomoiros Alexandros <kalomiros7@gmail.com>
2018-03-10 11:06:24 +0200

    real total order tester java file in test

--- test/TotalOrderTester.java
+++ test/TotalOrderTester.java
@@ -10,66 +10,53 @@ import java.io.FileInputStream;
 
 
 
-public class FifoTester {
+public class TotalOrderTester {
 
-    public static HashMap<String, Queue<String>> messages = new HashMap<>();
+    public static Queue<Queue<String>> messages = new LinkedList<>();
     
 
     public static void main(String[] args) throws IOException{
 
         // for all input files we read the messages
         for (String filename : args) {
+            Queue<String> q = new LinkedList<>();
             // open file
             BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
             String line = br.readLine();
             while (line != null) {
                 // parse the input message and then put it in messages
-
-                Scanner s = new Scanner(line);
-                int seq = Integer.parseInt(s.useDelimiter(Pattern.compile("[a-zA-Z]")).next());
-                String username = s.useDelimiter(" ").next();
-                String message = s.nextLine();
-                
-                Queue<String> q = messages.get(username);
-                if (q == null)
-                    q = new LinkedList<>();
-                
-                q.add(message);
-
-                if (!messages.containsKey(username))
-                    messages.put(username, q);
-                
+                q.add(line);
                 line = br.readLine();
             }
+            messages.add(q);
         }
 
-        // now we read from stdin
+        Queue<String>[] msgs = messages.toArray(new Queue[messages.size()]);
+        for (int i = 0; i < msgs.length; i++) {
+            for (int j = i + 1; j < msgs.length; j++) {
+               // first we need to create duplicates of the original data
+                Queue<String> q1 = new LinkedList(msgs[i]);
+                Queue<String> q2 = new LinkedList(msgs[j]);
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-        while (line != null) {
-            Scanner s = new Scanner(line);
-            int seq = Integer.parseInt(s.useDelimiter(Pattern.compile("[a-zA-Z]")).next());
-            String username = s.useDelimiter(" ").next();
-            String message = s.nextLine();
+                // now we remove any unique messages
+                for (String s : q1.toArray(new String[q1.size()])) {
+                    if (!q2.contains(s))
+                        q1.remove(s);
+                }
 
-            // we retreive this user's messages
-            Queue<String> q = messages.get(username);
-            if (q == null || q.isEmpty()) {
-                System.out.println("Fifo test failed");
-                return;
+               for (String s : q2.toArray(new String[q2.size()])) {
+                    if (!q1.contains(s))
+                        q2.remove(s);
+                }
+
+
+                // then the two queues have to be equal
+
+                if (!q1.equals(q2)) {
+                    System.out.println("Total order test failed");
+                    return;
+                }
             }
-
-            while (!q.isEmpty() && !q.peek().equals(message))
-                q.remove();
-            
-            if (q.isEmpty()) {
-                System.out.println("Fifo test failed");
-                return;
-            }
-
-            q.remove();
-            line = br.readLine();
         }
     }
 }
